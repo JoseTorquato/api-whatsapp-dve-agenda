eab046c046d66975caf017764104280e99efe61b
José Lucas <jose.torquato@grupoboticario.com.br>
2022-07-28 10:19:37 -0300

    first commit

---
+++ src/api/middlewares/loginCheck.js
@@ -0,0 +1,16 @@
+function loginVerification(req, res, next) {
+    if (!key) {
+        return res
+            .status(403)
+            .send({ error: true, message: 'no key query was present' })
+    }
+    const instance = WhatsAppInstances[key]
+    if (!instance.instance?.online) {
+        return res
+            .status(401)
+            .send({ error: true, message: "phone isn't connected" })
+    }
+    next()
+}
+
+module.exports = loginVerification
